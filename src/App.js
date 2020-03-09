24698f493994b750204a3060449a0c83e5ea6371
ramon cantanhede gusmão <ramoncgusmao@gmail.com>
2020-03-09 00:10:43 -0300

    primeiros passos com react, criando uma soma

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    numero1 : '',
+    numero2 : '',
+    resultado: ''
+  }
+
+  somar = () => {
+    const resultado =  parseInt(this.state.numero1) + parseInt(this.state.numero2);
+    this.setState({resultado: resultado})
+  }
+  render(){
+    return(
+      <div>
+        <label>numero1:</label>
+        <input type="text" value={this.state.numero1} onChange={(e) => this.setState({numero1: e.target.value})}  />
+        <br/>
+        <label>numero2:</label>
+        <input type="text" value={this.state.numero2} onChange={(e) => this.setState({numero2: e.target.value})}  />
+        <br/>
+        <button onClick={this.somar}>somar</button>
+        <br/>
+        o resultado é {this.state.resultado}
+      </div>
+    )
+  }
 }
 
 export default App;
