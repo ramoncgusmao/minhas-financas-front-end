20059d8ea7af4b43f872e0d6d0e12b6ce3e62c5f
ramon cantanhede gusmão <ramoncgusmao@gmail.com>
2020-03-09 14:18:39 -0300

    criando a tela de login

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
 
+import Login from './views/login';
+import 'bootswatch/dist/flatly/bootstrap.css';
+
 class App extends React.Component {
-
-  state = {
-    numero1 : '',
-    numero2 : '',
-    resultado: ''
-  }
-
-  somar = () => {
-    const resultado =  parseInt(this.state.numero1) + parseInt(this.state.numero2);
-    this.setState({resultado: resultado})
-  }
+ 
+ 
   render(){
     return(
-      <div>
-        <label>numero1:</label>
-        <input type="text" value={this.state.numero1} onChange={(e) => this.setState({numero1: e.target.value})}  />
-        <br/>
-        <label>numero2:</label>
-        <input type="text" value={this.state.numero2} onChange={(e) => this.setState({numero2: e.target.value})}  />
-        <br/>
-        <button onClick={this.somar}>somar</button>
-        <br/>
-        o resultado é {this.state.resultado}
-      </div>
-    )
+   <div>
+     <Login />
+   </div>)
   }
 }
 
-export default App;
+export default App
